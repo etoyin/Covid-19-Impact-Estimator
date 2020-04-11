3a590a875e285141c18a4018dbcad82416988b73
Toyin Adesina <toyinadesina60@gmail.com>
2020-04-11 16:09:58 +0100

    Updates

---
+++ precache-manifest.59eb883a560fdd7e24b8e8ea931104c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a540c6f1eed209522bb3e01107aeb60",
+    "url": "/covid-19-impact-estimator/index.html"
+  },
+  {
+    "revision": "4a053c63c95a3028dd86",
+    "url": "/covid-19-impact-estimator/static/css/main.025a1887.chunk.css"
+  },
+  {
+    "revision": "2d01eaa782e1d5b8d231",
+    "url": "/covid-19-impact-estimator/static/js/2.96862d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-impact-estimator/static/js/2.96862d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a053c63c95a3028dd86",
+    "url": "/covid-19-impact-estimator/static/js/main.fbf6717a.chunk.js"
+  },
+  {
+    "revision": "e56f3ace340e12f7ef4d",
+    "url": "/covid-19-impact-estimator/static/js/runtime-main.323afbd5.js"
+  }
+]);
